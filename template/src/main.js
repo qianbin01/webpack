7e76c28de3113d5332cd78ba80279d4060918f0b
Qian Bin <236490794@qq.com>
2019-01-05 10:50:43 +0800

    Update main.js

--- template/src/main.js
+++ template/src/main.js
@@ -2,12 +2,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
-import Vue from 'vue'
 import App from './App'
 {{#router}}
 import router from './router'
-{{/router}}
+  import './icons'
 
+{{/router}}
+const Vue = require('vue')
+const ElementUI = require('element-ui')
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

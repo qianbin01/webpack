d1f3b607bc8e43f588abcf4384c1a116f6365ce9
qb <236490794@qq.com>
2019-01-12 17:14:11 +0800

    update lib

--- template/src/lib/subLib/otherUtils.js
+++ template/src/lib/subLib/otherUtils.js
@@ -118,7 +118,63 @@ const countDown = (flag, fn, begin = 60, end = 0, step = 1) => {
     }
   }, step * 1000);
 }
-
+/**
+ *
+ * 压缩图片
+ * @param {*} path 图片路径
+ * @param {*} callback  压缩完的bas4图片
+ * @param {number} [quality=1] 压缩质量
+ */
+const compressImage = (path, callback, quality = 1) => {
+  let img = new Image();
+  img.src = path;
+  img.onload = function () {
+    //默认按比例压缩
+    if (this.width < 120) {
+      callback(path);
+    }
+    let scale = this.height / this.width;
+    let w = this.width / 20;
+    if (w < 120) {
+      w = 120
+    }
+    let h = w * scale;
+    //生成canvas
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    // 创建属性节点
+    canvas.setAttribute("width", w);
+    canvas.setAttribute("height", h);
+    ctx.drawImage(this, 0, 0, w, h);
+    let base64Data = canvas.toDataURL('image/jpeg', quality);
+    callback(base64Data);
+    // quality值越小，所绘制出的图像越模糊
+  };
+}
+/**
+ *
+ * base64转Blob(图片)  常用于上传
+ * @param {*} base64 待转的bae64对象
+ * @returns Blob 对象
+ */
+const convertBase64ToBlob = (base64) => {
+  let base64Arr = base64.split(',');
+  let imgType = '';
+  let base64String = '';
+  if (base64Arr.length > 1) {
+    base64String = base64Arr[1];
+    imgType = base64Arr[0].substring(base64Arr[0].indexOf(':') + 1, base64Arr[0].indexOf(';'));
+  }
+  let bytes = atob(base64String);
+  let bytesCode = new ArrayBuffer(bytes.length);
+  let byteArray = new Uint8Array(bytesCode);
+  for (let i = 0; i < bytes.length; i++) {
+    byteArray[i] = bytes.charCodeAt(i);
+  }
+  return new Blob([bytesCode], {
+    type: imgType
+  });
+};
 
 export default {
   areaRandom,
@@ -127,5 +183,7 @@ export default {
   throttle,
   scrollMore,
   isPhone,
-  countDown
+  countDown,
+  compressImage,
+  convertBase64ToBlob
 }

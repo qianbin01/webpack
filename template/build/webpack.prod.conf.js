7327bd17095bf6fe674d1c201a7237bf4700d28b
qb <236490794@qq.com>
2019-01-29 10:18:38 +0800

    git commit  file

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -102,7 +102,7 @@ if (config.build.productionGzip) {
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
+      filename: '[path].gz[query]',
       algorithm: 'gzip',
       test: new RegExp(
         '\\.(' +
